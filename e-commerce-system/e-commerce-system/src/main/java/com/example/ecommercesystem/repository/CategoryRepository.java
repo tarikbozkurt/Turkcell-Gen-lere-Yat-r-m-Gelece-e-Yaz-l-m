4f6622c45f6960c8ace20c280bc6cd5ffdee133a
Tarık Bozkurt <tarikbzkrt.fu@gmail.com>
2023-04-01 00:10:46 +0300

    Delete e-commerce-system directory

--- e-commerce-system/e-commerce-system/src/main/java/com/example/ecommercesystem/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.ecommercesystem.repository;
-
-import com.example.ecommercesystem.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category,Long> {
-
-}
